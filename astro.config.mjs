9a780e17a7c4fb441f46b9b84a84d4401794a97c
Rafael <rafael_dvid@hotmail.com>
2023-11-07 15:58:43 -0300

    fix responsiveness and implement sidebar cardhover

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+import { defaultLang } from './src/i18n/ui';
 
 // https://astro.build/config
 export default defineConfig({
+  redirects:{
+    '/': `/${defaultLang}`
+  },
   integrations: [tailwind()]
 });
