55cfc8ecae049676d54da3f134b7799edf51cc08
Rafael <rafael_dvid@hotmail.com>
2024-02-21 22:52:28 -0300

    astro-icons setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,20 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 import { defaultLang } from './src/i18n/ui';
+import icon from "astro-icon";
 
 export default defineConfig({
   site: 'https://rdvid.tech',
   redirects:{
     '/': `/${defaultLang}`
   },
-  integrations: [tailwind()]
+  integrations: [
+    tailwind(), 
+    icon({
+      include: {
+        fa: ["*"], 
+        faBrands: ["*"] 
+      },
+    })
+  ]
 });
